79349e53893d4acc7e0e5c87096b12d0209da8dd
Tri Vuong <trivektor@gmail.com>
2011-08-17 19:15:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatingOverMeal::Application.config.secret_token = '80b224b66105271f7b1ba5de549fad73832adc2b811ec2bef924d08771a122eaad0c2e1912d03d03527c68c409795f634c83172b65e4444e9b1e65ad3356c306'
