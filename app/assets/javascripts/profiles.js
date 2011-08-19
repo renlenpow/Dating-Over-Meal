944e96c63936caa4202c8f2260e8d289bd1f513b
Tri Vuong <trivektor@gmail.com>
2011-08-18 20:28:47 -0700

    Edit and show profile

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $("#profile_information_form").bind("ajax:success", function(evt, data, status, xhr){
+    //alert(xhr.responseText)
+  })
+})
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,23 @@
 class ProfilesController < ApplicationController
   
+  before_filter :load_profile
+  
   def show
+    
   end
   
   def edit
   end
   
   def update
-    profile = Profile.find(params[:id])
-    profile.update_attributes(params[:profile])
-    render :nothing => true
+    @profile.update_attributes(params[:profile])
+    render :json => {:success => 1}
+  end
+  
+  private
+  
+  def load_profile
+    @profile = Profile.find(params[:id])
   end
   
 end
