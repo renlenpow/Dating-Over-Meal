6ca6ff577eaa10b29696d3f6c7296f704dfe0f6a
Tri Vuong <trivektor@gmail.com>
2011-08-31 10:31:43 -0700

    place street view

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $(".back_to_top").click(function(){
     $.scrollTo(0, 500);
   })
+  
+  $(".tabs li").click(function(){
+    $(this).addClass("current").siblings().removeClass("current")
+    $(".tab_content").hide()
+    $("#" + $(this).attr("tab")).show()
+  })
 })
